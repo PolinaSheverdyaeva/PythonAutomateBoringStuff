1549bc1856434d326f33498b912514c4bd193690
Polina Sheverdyaeva <sheverdyaeva@gmail.com>
2025-04-12 23:21:48 -0400

    commit again

--- Hello.py
+++ Hello.py
@@ -1,1 +1,16 @@
-print("Hello")
+print("Hello World!")
+print("What is your name?")
+myName = input().strip().capitalize() #strip spaces and capitalize
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+print('What is your age?')
+myAge = input().strip()
+if myAge.isdigit():
+    myAge = int(myAge)
+else:
+    print("Error")
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+print('spam' + 'spamspam')
+print('spam' * 3)
+
--- lesson1.py
+++ lesson1.py
@@ -30,3 +30,26 @@ print('Your age is : ' + str(number))
 
 from lesson2 import sum_it
 print(sum_it(100, 200))
+
+print(2 + 3 * 6)
+print((2 + 3) * 6)
+print(2 ** 8)
+print(23 / 7)
+print(23 // 7)
+print(23 % 7)
+print((5 - 1) * ((7 + 1) / (3 - 1)))
+
+# Spam variable
+spam = 40
+print('Spam ' + str(spam))
+eggs = 2
+print('Eggs ' + str(eggs))
+print(f'spam + eggs = {spam + eggs}')
+print(f'spam + eggs + spam = {spam + eggs + spam}')
+print(f'spam = spam + 2 = {spam + 2}')
+spam = 'Hello'
+print(f'spam = {spam}')
+spam = 'Goodbye'
+print(f'spam = {spam}')
+
+
